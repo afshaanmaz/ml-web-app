de73b94b5da0f81c066dcbca0a272482cfde6d42
Rachana Sreedhar <rsreedhar@wayfair.com>
2020-01-31 13:46:49 -0500

    Deleted test.txt file. Added comments to ml_app. Changed route home() to hello_world

--- ml_app.py
+++ ml_app.py
@@ -1,12 +1,32 @@
 """Basic app to demonstrate anatomy of flask"""
 
 from flask import Flask, render_template, request
-import archive_sentiment_analysis.preprocess_data as preprocess
-import archive_sentiment_analysis.test as test
+import sentiment_analysis.api_sentiment_analysis as senti_api
 app = Flask(__name__)
 
 
+@app.route('/predict')
+def predict():
+    """ Preprocess and pass the user input to the ML api. Display the prediction with a render template """
+
+    # Step 3c-1:
+    user_input = accept_user_input()
+    print('User input = ', user_input)
+
+    # Preprocess input - Remember to import the file from sentiment analysis
+    preprocessed_data = senti_api.preprocess_reviews(user_input)
+
+    # Step 3c-2
+    # Send the preprocessed data to the ML model
+    sentiment = test.inference(preprocessed_data)
+
+    # Send the prediction to the web app
+    return render_template('predict.html', predicted_out=sentiment)
+
+
 def accept_user_input():
+    """Accept user input with forms"""
+
     if request.args:
         user_input = request.args['user_review']
     else:
@@ -17,6 +37,7 @@ def accept_user_input():
 
 @app.route('/display_data')
 def display_data():
+    """Accept user input and display the user input using render template"""
 
     # Step 3b-2
     # Accept user input and print to html
@@ -26,29 +47,14 @@ def display_data():
 
 
 @app.route('/')
-def home():
+def hello_world():
+    """Basic function for simply displaying a statement via Flask"""
+
     # Step 3a:
     # return 'Hello from Flask!'
 
     # Step 3b-1:
     return render_template('home.html')
-
-
-@app.route('/predict')
-def predict():
-    # Step 3c-1:
-    user_input = accept_user_input()
-    print('User input = ', user_input)
-
-    # Preprocess input - Remember to import the file from sentiment analysis
-    preprocessed_data = preprocess.preprocess_reviews(user_input)
-
-    # Step 3c-2
-    # Send the preprocessed data to the ML model
-    sentiment = test.inference(preprocessed_data)
-
-    # Send the prediction to the web app
-    return render_template('predict.html', predicted_out=sentiment)
 
 
 if __name__ == '__main__':
